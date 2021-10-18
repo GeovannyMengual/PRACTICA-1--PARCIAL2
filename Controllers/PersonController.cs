de1b9c524ccb7858613ca4275f04228635534b6e
GeovannyMengual <91901655+GeovannyMengual@users.noreply.github.com>
2021-10-17 23:12:01 -0500

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using QueryApi.Repositories;
+using QueryApi.Domain;
+
+
+namespace PRACTICA1_PARCIAL2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            var repository = new PersonRepository();
+            var persons = repository.GetAll();
+            return Ok(persons);
+        }
+    }
+}
---
+++ Domain/Person.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace QueryApi.Domain
+{
+    //Este apartado para nuestra consulta de datos, por  lo tanto declaramo de la siguiente manera
+    public record Person(int Id, string FirstName, string LastName, string Email, char Gender, string Job, int Age);
+}
